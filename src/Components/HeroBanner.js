d63978bda8d75b8c49721c3b05a6b0d7cf1ba0a2
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-05-09 17:07:10 +0300

    commit

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import video from "../Assets/Videos/heroBannerVideo.mp4";
 
 const HeroBanner = () => {
     return (
-        <div>
-            This is hero banner.
+        <div className="video-container">
+            <ReactPlayer
+                url={video}
+                playing
+                loop
+                muted
+                cover
+                height
+                width
+                className="heroBannerVideo"
+            />
         </div>
     );
 };
