d37b2d45d220fee25b11a704712c6f7a268f9135
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-05-09 19:00:30 +0300

    commit

--- src/Components/AddRecipes.js
+++ src/Components/AddRecipes.js
@@ -5,7 +5,36 @@ const AddRecipes = () => {
     return (
         <>
             <Nav/>
-            Adding new recipe
+            <h2>Adding new recipe</h2>
+            <form>
+                <label for="name">Name</label>
+                <input type="text" name="name"></input>
+
+                <label for="author">Author</label>
+                <input type="text" name="author"></input>
+
+                <label for="country">Recipe is from: </label>
+                <select name="cars" id="cars">
+                    <option value=""></option>
+                </select>
+
+                <label for="description">Description</label>
+                <textarea type="text" name="description"></textarea>
+
+                <label for="image">Image</label>
+                <input type="text" name="image"></input>
+
+                <label for="quantity">Quantity</label>
+                <input type="text" name="quantity"></input>
+
+                <label for="ingredient">Ingredient</label>
+                <input type="text" name="ingredient"></input>
+
+                <label for="instructions">Instructions</label>
+                <textarea type="text" name="instructions"></textarea>
+
+                <button type="submit">Post Recipe</button>
+            </form>
         </>
     );
 };
