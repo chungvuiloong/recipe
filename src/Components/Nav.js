10e5d059b0ce3b636a83cb158156376e26551dc8
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-05-09 12:53:25 +0300

    commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav>
-            
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/pokelist">PokeList</Link></li>
+                <li><Link to="/favlist">FavList</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
         </nav>
     );
 };
