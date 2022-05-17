aebb8316b1095e96e5515749ffb29650f63afed5
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-05-17 12:06:37 +0300

    commit

--- src/Components/AddRecipes.js
+++ src/Components/AddRecipes.js
@@ -4,36 +4,25 @@ import Nav from './Nav';
 
 const AddRecipes = () => {
 
-    const [data, setData] = useState([{
+    const [data, setData] = useState({
         name: '',
         author: '',
-        // country: '',
-        // description: '',
-        // image: '',
-        // instructions: '',
-        // ingredients: []
-    }]);
-
-    // const [inputData, setInputData] = useState([{
-    //     name: '',
-    //     author: '',
-    //     country: '',
-    //     description: '',
-    //     image: '',
-    //     instructions: '',
-    //     ingredients: []
-    // }]);
+        country: '',
+        description: '',
+        image: '',
+        instructions: '',
+        ingredients: []
+    });
 
     const [countries, setCountries] = useState([]);
     const [ingredients, setIngredients] = useState([
         { 
             id: 1, 
-            ingredient: 'testingLemon',
-            quantity: 'several lemons' 
+            ingredient: '',
+            quantity: '' 
         },
       ]);
-    
-    
+
     // //Getting all the countries
     useEffect(() => {
     axios.get(`https://restcountries.com/v3.1/all`)
@@ -47,12 +36,9 @@ const AddRecipes = () => {
 
     // {...this.state.inputData}
     function submitHandler (e){
-        e.preventDefault();
+        // e.preventDefault();
         console.log("Submit data");
-        axios.post("http://localhost:3333/recipes", ...data)
-        .then((res) => console.log('res', res))
-        .catch((err) => console.log('error',err));
-        // closeHandler();
+        axios.post("http://localhost:3333/recipes", data, ...ingredients);
         console.log(data);
     };
 
@@ -62,85 +48,91 @@ const AddRecipes = () => {
         setIngredients([...ingredients, { id: ingredients.length + 1, ingredient: '', quantity: '' }]);
     }
 
-    // function deleteHandler (e) {
-    //     e.preventDefault();
-    //     console.log("Delete handler");
-    // }
+    function deleteHandler (e) {
+        e.preventDefault();
+        console.log("Delete handler");
+    }
 
     // function closeHandler () {
     //     window.location.reload();
     //   };
 
-    function getInputValue (e) {
+    // Checks for the inputted value
+    // function getInputValue (e) {
+    //     const name = e.target.name;
+    //     const value = e.target.value;
+    //     console.log(name, value);
+    // };
+
+    const inputHandler = (e) => {
         const name = e.target.name;
-        const userValue = e.target.value;
-        console.log(name, userValue);
-   
+        const value = e.target.value;
+        setData({ ...data, [name]: value });
     };
 
-    // inputData: { ...this.state.inputData, [e.target.name]: e.target.value },
-
-    // const inputHandler = (e) => {
-    //     setInputData({
-    //       ...inputData,
-    //       [e.target.name]: e.target.value,
-    //     });
-    //   };
-
-      const changeData = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-      };
+    const changeIncData = (e, i) => {
+        const { name, value } = e.target;
+        const incList = [...ingredients];
+        incList[i][name] = value;
+        setIngredients(incList);
+        setData({ ...data, ingredients: ingredients });
+    };
 
     return (
         <>
             <Nav/>
             <form className="form-container" 
-            onChange={getInputValue} shows the inoput value
+            // onChange={getInputValue} //shows the input value
+            onSubmit={submitHandler}
             >
                 <h2>Adding new recipe</h2>
                 
                 <label htmlFor="name">Name of recipe</label>
-                <input type="text" name="name" onChange={changeData}></input>
+                <input type="text" name="name" id="name" onChange={inputHandler}></input>
 
-               <label htmlFor="author">Author</label>
-                <input type="text" name="author" onChange={changeData} ></input>
+                <label htmlFor="author">Author</label> 
+                <input type="text" name="author" id="author" onChange={inputHandler}></input> 
 
-{/*                 
+               
                 <label htmlFor="country">Recipe is from: </label>
-                <select name="country" id="country">
+                <select name="country" id="country" onChange={inputHandler}>
                     
                     {sortedCountries?.map(
                         (country) => 
                             <option value={country.name.common} 
-                                    key=  {country.name.common}>
-                                          {country.flag}{country.name.common}
+                                    key=  {country.name.common}
+                                    name={country.name.common}
+                                    >
+                                          {country.flag} {country.name.common}
                             </option>
                     )}
                 </select>
 
                 <label htmlFor="description">Description</label>
-                <textarea type="text" name="description"></textarea>
+                <textarea type="text" name="description" onChange={inputHandler}></textarea>
 
                 <label htmlFor="image">Image</label>
-                <input type="text" name="image"></input>
+                {/* <input type="text" name="image" onChange={inputHandler}></input> */}
+                <input type="file" name="image" onChange={inputHandler}></input>
                 
                 <div>Ingredients</div>
                 
                     {
-                    ingredients?.map((ingredient) => (                        
-                        <div key={ingredient.ingredient}>
-                            <input type="text" name="quantity" placeholder="quantity" defaultValue={ingredient.quantity}/>
-                            <input type="text" name="ingredient" placeholder="ingredient" defaultValue={ingredient.ingredient}/>
+                    ingredients?.map((_, i) => (                        
+                        <div key={i}>
+                            <input type="text" name="quantity" placeholder="quantity" onChange={(e) => changeIncData(e, i)}/>
+                            <input type="text" name="ingredient" placeholder="ingredient" onChange={(e) => changeIncData(e, i)}/>
                             <button type="button" name="delete" onClick={deleteHandler}>Delete</button>
                         </div>
 
                     ))}
                 
                 <button type="submit" onClick={addIngredient}>Add more ingredients</button>
-                <label htmlFor="instructions">Instructions</label>
-                <textarea type="text" name="instructions"></textarea> */}
 
-                <button type="submit" onClick={submitHandler}>Post Recipe</button>
+                <label htmlFor="instructions">Instructions</label>
+                <textarea type="text" name="instructions" onChange={inputHandler}></textarea>
+
+                <button type="submit">Post Recipe</button> 
             </form>
         </>
     );
